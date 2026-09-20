4bd15ff343015bc5e7152d047fb3fc0256eea9e9
Karenina <eadkarenina@gmail.com>
2026-09-20 16:05:14 -0300

    Feature: adiciona idade à pessoa

--- src/exemplobranches/ExemploBranches.java
+++ src/exemplobranches/ExemploBranches.java
@@ -4,9 +4,10 @@ public class ExemploBranches {
 
     public static void main(String[] args) {
 
-        Pessoa p = new Pessoa("Kare");
+        Pessoa p = new Pessoa("Kare", 53);
         
         System.out.println("Nome: " + p.getNome());
+        System.out.println("Idade: " + p.getIdade());   
     }
 
 }
