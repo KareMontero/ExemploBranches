c0dd2a7186c1362cb7fb3779fe4816d702a23afe
Karenina <eadkarenina@gmail.com>
2026-09-20 09:14:57 -0300

    feature: cria estrutura inicial do projeto

---
+++ src/exemplobranches/ExemploBranches.java
@@ -0,0 +1,12 @@
+package exemplobranches;
+
+public class ExemploBranches {
+
+    public static void main(String[] args) {
+
+        Pessoa p = new Pessoa("Kare");
+        
+        System.out.println("Nome: " + p.getNome());
+    }
+
+}
---
+++ src/exemplobranches/Pessoa.java
@@ -0,0 +1,25 @@
+package exemplobranches;
+
+public class Pessoa {
+
+    private String nome;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+   
+    
+    
+}
